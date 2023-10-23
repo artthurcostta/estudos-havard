56d7b2329652e589397fd6142b0442cc2f12534a
artthurcostta <artthurcosttamattos@gmail.com>
2023-10-23 00:30:04 -0300

    troquei pra exibir com mais exatidão a nota

--- enem.py
+++ enem.py
@@ -1,9 +1,9 @@
 def main():
-    x=int(input("Linguagens, Códigos e suas Tecnologias="))
-    y=int(input("Ciências Humanas e suas Tecnologias"))
-    z=int(input("Ciências da Natureza e suas Tecnologias"))
-    a=int(input("Matemática e suas Tecnologias"))
-    b=int(input("Redação"))
+    x=float(input("Linguagens, Códigos e suas Tecnologias="))
+    y=float(input("Ciências Humanas e suas Tecnologias"))
+    z=float(input("Ciências da Natureza e suas Tecnologias"))
+    a=float(input("Matemática e suas Tecnologias"))
+    b=float(input("Redação"))
     name=x+y+z+a+b
-    print(name//2)
+    print(f"Nota={name:.2f}")
 main()
