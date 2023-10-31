d96f11d6242332f8d7e8956f24d31ac3b9c57611
artthurcostta <artthurcosttamattos@gmail.com>
2023-10-31 05:57:09 -0300

    estudos havard

---
+++ meal.py
@@ -0,0 +1,20 @@
+def main():
+    akarasta = input("What time is it")
+    time = convert(akarasta)
+    if time >=7 and time<=8:
+        print("breakfast time")
+    elif time >=12 and time <=13:
+        print("lunch time")
+    elif time >=18 and time <=19:
+        print("dinner time")
+    else:
+        print("VAI TOMAR NO CU")
+
+def convert(time):
+    horas, minutos = time.split(":")
+    minutos = float(minutos) / 60
+    return float(horas) + minutos
+
+
+if __name__ == "__main__":
+    main()
