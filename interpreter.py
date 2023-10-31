24fb23e81f9ce2c3ed071fb3a595a952f03ccd37
artthurcostta <artthurcosttamattos@gmail.com>
2023-10-31 05:44:44 -0300

    estudos havard

---
+++ interpreter.py
@@ -0,0 +1,11 @@
+x,y,z = input("Expression ").strip().strip()
+if y == "*":
+    print(x*z)
+elif y == "+":
+    print(x+z)
+elif y == "-":
+    print(x-z)
+elif y =="/":
+    print(x/z)
+else:
+    print("coloque algo direito seu safado")
